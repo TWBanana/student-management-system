5ad95e6f2240792df83e2fc39c8e70b6d1bb6340
TWBanana <a45068798@gmail.com>
2021-05-16 16:18:25 -0400

    make test fail

--- src/test/java/com/kh388/StudentManagementSystemApplicationTests.java
+++ src/test/java/com/kh388/StudentManagementSystemApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kh388;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class StudentManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Ooops test failed");
 	}
 
 }
