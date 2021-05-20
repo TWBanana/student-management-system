1a107a1832790a994fdddaa27b8bc565959b9d51
TWBanana <a45068798@gmail.com>
2021-05-19 23:14:04 -0400

    add unit test and integration test

--- src/test/java/com/kh388/StudentManagementSystemApplicationTests.java
+++ src/test/java/com/kh388/StudentManagementSystemApplicationTests.java
@@ -1,13 +1,35 @@
 package com.kh388;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 class StudentManagementSystemApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+
+		Calculator underTest = new Calculator();
+
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		// when
+		int result = underTest.add(numberOne, numberTwo);
+
+		// then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+
 	}
+
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
+	}
+
 
 }
